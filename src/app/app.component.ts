2257936020d3c68b23a97ebd0fc9c87799e850de
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2025-04-09 10:32:44 +0530

    create taskmanager form to running step

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TaskFormComponent} from './components/task-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TaskFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
